6c77f4583424844b633c066d4b07013a5dc0e335
SC\josh.osborne <jaosborne4@gmail.com>
2017-11-01 17:35:59 -0500

    l

---
+++ Resources/navigation.php
@@ -0,0 +1,12 @@
+<nav>
+    <ul>
+       <li><a href = "file:///C:/Users/katie.cardoza/Desktop/localphotography/public_html/index.php">Home</a></li>
+       <li><a href = "file:///C:/Users/katie.cardoza/Desktop/localphotography/public_html/classes.php">Classes</a></li>
+       <li><a href = "file:///C:/Users/katie.cardoza/Desktop/localphotography/public_html/Studio%20Hours.php">Studio Hours</a></li>
+       <li><a href = "file:///C:/Users/katie.cardoza/Desktop/localphotography/public_html/Studio%20History.php">Studio History</a></li>
+       <li><a href = "file:///C:/Users/katie.cardoza/Desktop/localphotography/public_html/internships.php">Internships</a></li>
+       <li><a href = "file:///C:/Users/katie.cardoza/Desktop/localphotography/public_html/about.php">About</a></li>
+       <li><a href = "file:///C:/Users/katie.cardoza/Desktop/localphotography/public_html/login.php">Log In</a></li>
+       <li><a href = "file:///C:/Users/katie.cardoza/Desktop/localphotography/public_html/signup.php">Sign Up</a></li>
+    </ul>
+</nav>
